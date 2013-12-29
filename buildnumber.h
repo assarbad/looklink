835cce138c4f951ba0c90c06787aa7787f389662
Oliver Schneider <oliver@assarbad.net>
2013-12-29 01:29:22 +0000

    - Further adjustments and automation of release packaging

--- buildnumber.h
+++ buildnumber.h
@@ -1,3 +1,3 @@
 // Automatically created file! 
-#define _FILE_VERSION_BUILD 112 
+#define _FILE_VERSION_BUILD 113 
  
--- relbuild.cmd
+++ relbuild.cmd
@@ -1,5 +1,18 @@
 @echo off
+if "%~1" == "noinc" set NO_INCREASE_BUILDNO=1
 call ddkbuild.cmd -WNET2K fre . -cZ
 set NO_INCREASE_BUILDNO=1
 call ddkbuild.cmd -WNETAMD64 fre . -cZ
+if exist bin\*.idb del /f bin\*.idb
 call ollisign.cmd "%~dp0\bin\*.exe" "http://assarbad.net" "Looklink, application to inspect reparse points of various kinds ..."
+:: Find 7-Zip
+set SEVENZIP=%ProgramFiles%\7-Zip\7z.exe
+if not exist "%SEVENZIP%" set SEVENZIP=%ProgramFiles(x86)%\7-Zip\7z.exe
+if not exist "%SEVENZIP%" ( echo ERROR: Could not find 7z.exe & goto :EOF )
+set ARCHIVE=looklink.zip
+if exist %ARCHIVE% del /f %ARCHIVE%
+if exist %ARCHIVE%.asc del /f %ARCHIVE%.asc
+"%SEVENZIP%" a -tzip %ARCHIVE% bin BUILD makefile sources *.cmd *.cpp *.h *.rc *.rst *.sln *.vcproj *.vcxproj *.vsprops
+sha1sum %ARCHIVE%
+md5sum %ARCHIVE%
+gpg -a --detach-sign %ARCHIVE%
